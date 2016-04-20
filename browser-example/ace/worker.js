1fefc04f8e980754aa3444d26dc57c4eeecb6a4e
Michael Maier <maier1michael@gmail.com>
2016-04-20 10:34:13 +0200

    un-minify worker.js

--- browser-example/ace/worker.js
+++ browser-example/ace/worker.js
@@ -1,119 +1,219 @@
+// code from: https://raw.githubusercontent.com/ajaxorg/ace/master/lib/ace/worker/worker.js
 "no use strict";
-(function(e) {
-  if (typeof e.window != "undefined" && e.document) return;
-  e.console = function() {
-    var e = Array.prototype.slice.call(arguments, 0);
-    postMessage({
-      type: "log",
-      data: e
-    })
-  }, e.console.error = e.console.warn = e.console.log = e.console.trace = e.console, e.window = e, e.ace = e, e.onerror = function(e, t, n, r, i) {
-    postMessage({
-      type: "error",
-      data: {
-        message: e,
-        file: t,
-        line: n,
-        col: r,
-        stack: i.stack
-      }
-    })
-  }, e.normalizeModule = function(t, n) {
-    if (n.indexOf("!") !== -1) {
-      var r = n.split("!");
-      return e.normalizeModule(t, r[0]) + "!" + e.normalizeModule(t, r[1])
-    }
-    if (n.charAt(0) == ".") {
-      var i = t.split("/")
-        .slice(0, -1)
-        .join("/");
-      n = (i ? i + "/" : "") + n;
-      while (n.indexOf(".") !== -1 && s != n) {
-        var s = n;
-        n = n.replace(/^\.\//, "")
-          .replace(/\/\.\//, "/")
-          .replace(/[^\/]+\/\.\.\//, "")
-      }
-    }
-    return n
-  }, e.require = function(t, n) {
-    n || (n = t, t = null);
-    if (!n.charAt) throw new Error("worker.js require() accepts only (parentId, id) as arguments");
-    n = e.normalizeModule(t, n);
-    var r = e.require.modules[n];
-    if (r) return r.initialized || (r.initialized = !0, r.exports = r.factory()
-      .exports), r.exports;
-    var i = n.split("/");
-    if (!e.require.tlns) return console.log("unable to load " + n);
-    i[0] = e.require.tlns[i[0]] || i[0];
-    var s = i.join("/") + ".js";
-    return e.require.id = n, importScripts(s), e.require(t, n)
-  }, e.require.modules = {}, e.require.tlns = {}, e.define = function(t, n, r) {
-    arguments.length == 2 ? (r = n, typeof t != "string" && (n = t, t = e.require.id)) : arguments.length == 1 && (r = t, n = [], t = e.require.id);
-    if (typeof r != "function") {
-      e.require.modules[t] = {
-        exports: r,
-        initialized: !0
-      };
-      return
-    }
-    n.length || (n = ["require", "exports", "module"]);
-    var i = function(n) {
-      return e.require(t, n)
+;(function(window) {
+  if (typeof window.window != "undefined" && window.document)
+    return;
+  if (window.require && window.define)
+    return;
+
+  if (!window.console) {
+    window.console = function() {
+      var msgs = Array.prototype.slice.call(arguments, 0);
+      postMessage({type: "log", data: msgs});
     };
-    e.require.modules[t] = {
+    window.console.error =
+      window.console.warn =
+        window.console.log =
+          window.console.trace = window.console;
+  }
+  window.window = window;
+  window.ace = window;
+
+  window.onerror = function(message, file, line, col, err) {
+    postMessage({type: "error", data: {
+      message: message,
+      data: err.data,
+      file: file,
+      line: line,
+      col: col,
+      stack: err.stack
+    }});
+  };
+
+  window.normalizeModule = function(parentId, moduleName) {
+    // normalize plugin requires
+    if (moduleName.indexOf("!") !== -1) {
+      var chunks = moduleName.split("!");
+      return window.normalizeModule(parentId, chunks[0]) + "!" + window.normalizeModule(parentId, chunks[1]);
+    }
+    // normalize relative requires
+    if (moduleName.charAt(0) == ".") {
+      var base = parentId.split("/").slice(0, -1).join("/");
+      moduleName = (base ? base + "/" : "") + moduleName;
+
+      while (moduleName.indexOf(".") !== -1 && previous != moduleName) {
+        var previous = moduleName;
+        moduleName = moduleName.replace(/^\.\//, "").replace(/\/\.\//, "/").replace(/[^\/]+\/\.\.\//, "");
+      }
+    }
+
+    return moduleName;
+  };
+
+  window.require = function require(parentId, id) {
+    if (!id) {
+      id = parentId;
+      parentId = null;
+    }
+    if (!id.charAt)
+      throw new Error("worker.js require() accepts only (parentId, id) as arguments");
+
+    id = window.normalizeModule(parentId, id);
+
+    var module = window.require.modules[id];
+    if (module) {
+      if (!module.initialized) {
+        module.initialized = true;
+        module.exports = module.factory().exports;
+      }
+      return module.exports;
+    }
+
+    if (!window.require.tlns)
+      return console.log("unable to load " + id);
+
+    var path = resolveModuleId(id, window.require.tlns);
+    if (path.slice(-3) != ".js") path += ".js";
+
+    window.require.id = id;
+    window.require.modules[id] = {}; // prevent infinite loop on broken modules
+    importScripts(path);
+    return window.require(parentId, id);
+  };
+  function resolveModuleId(id, paths) {
+    var testPath = id, tail = "";
+    while (testPath) {
+      var alias = paths[testPath];
+      if (typeof alias == "string") {
+        return alias + tail;
+      } else if (alias) {
+        return  alias.location.replace(/\/*$/, "/") + (tail || alias.main || alias.name);
+      } else if (alias === false) {
+        return "";
+      }
+      var i = testPath.lastIndexOf("/");
+      if (i === -1) break;
+      tail = testPath.substr(i) + tail;
+      testPath = testPath.slice(0, i);
+    }
+    return id;
+  }
+  window.require.modules = {};
+  window.require.tlns = {};
+
+  window.define = function(id, deps, factory) {
+    if (arguments.length == 2) {
+      factory = deps;
+      if (typeof id != "string") {
+        deps = id;
+        id = window.require.id;
+      }
+    } else if (arguments.length == 1) {
+      factory = id;
+      deps = [];
+      id = window.require.id;
+    }
+
+    if (typeof factory != "function") {
+      window.require.modules[id] = {
+        exports: factory,
+        initialized: true
+      };
+      return;
+    }
+
+    if (!deps.length)
+    // If there is no dependencies, we inject "require", "exports" and
+    // "module" as dependencies, to provide CommonJS compatibility.
+      deps = ["require", "exports", "module"];
+
+    var req = function(childId) {
+      return window.require(id, childId);
+    };
+
+    window.require.modules[id] = {
       exports: {},
       factory: function() {
-        var e = this,
-          t = r.apply(this, n.map(function(t) {
-            switch (t) {
-              case "require":
-                return i;
-              case "exports":
-                return e.exports;
-              case "module":
-                return e;
-              default:
-                return i(t)
-            }
-          }));
-        return t && (e.exports = t), e
+        var module = this;
+        var returnExports = factory.apply(this, deps.map(function(dep) {
+          switch (dep) {
+            // Because "require", "exports" and "module" aren't actual
+            // dependencies, we must handle them seperately.
+            case "require": return req;
+            case "exports": return module.exports;
+            case "module":  return module;
+            // But for all other dependencies, we can just go ahead and
+            // require them.
+            default:        return req(dep);
+          }
+        }));
+        if (returnExports)
+          module.exports = returnExports;
+        return module;
       }
-    }
-  }, e.define.amd = {}, e.initBaseUrls = function(t) {
-    require.tlns = t
-  }, e.initSender = function() {
-    var n = e.require("ace/lib/event_emitter")
-        .EventEmitter,
-      r = e.require("ace/lib/oop"),
-      i = function() {};
-    return function() {
-      r.implement(this, n), this.callback = function(e, t) {
+    };
+  };
+  window.define.amd = {};
+  require.tlns = {};
+  window.initBaseUrls  = function initBaseUrls(topLevelNamespaces) {
+    for (var i in topLevelNamespaces)
+      require.tlns[i] = topLevelNamespaces[i];
+  };
+
+  window.initSender = function initSender() {
+
+    var EventEmitter = window.require("ace/lib/event_emitter").EventEmitter;
+    var oop = window.require("ace/lib/oop");
+
+    var Sender = function() {};
+
+    (function() {
+
+      oop.implement(this, EventEmitter);
+
+      this.callback = function(data, callbackId) {
         postMessage({
           type: "call",
-          id: t,
-          data: e
-        })
-      }, this.emit = function(e, t) {
+          id: callbackId,
+          data: data
+        });
+      };
+
+      this.emit = function(name, data) {
         postMessage({
           type: "event",
-          name: e,
-          data: t
-        })
-      }
-    }.call(i.prototype), new i
+          name: name,
+          data: data
+        });
+      };
+
+    }).call(Sender.prototype);
+
+    return new Sender();
   };
-  var t = e.main = null,
-    n = e.sender = null;
-  e.onmessage = function(r) {
-    var i = r.data;
-    if (i.command) {
-      if (!t[i.command]) throw new Error("Unknown command:" + i.command);
-      t[i.command].apply(t, i.args)
-    } else if (i.init) {
-      initBaseUrls(i.tlns), require("ace/lib/es5-shim"), n = e.sender = initSender();
-      var s = require(i.module)[i.classname];
-      t = e.main = new s(n)
-    } else i.event && n && n._signal(i.event, i.data)
-  }
+
+  var main = window.main = null;
+  var sender = window.sender = null;
+
+  window.onmessage = function(e) {
+    var msg = e.data;
+    if (msg.event && sender) {
+      sender._signal(msg.event, msg.data);
+    }
+    else if (msg.command) {
+      if (main[msg.command])
+        main[msg.command].apply(main, msg.args);
+      else if (window[msg.command])
+        window[msg.command].apply(window, msg.args);
+      else
+        throw new Error("Unknown command:" + msg.command);
+    }
+    else if (msg.init) {
+      window.initBaseUrls(msg.tlns);
+      require("ace/lib/es5-shim");
+      sender = window.sender = window.initSender();
+      var clazz = require(msg.module)[msg.classname];
+      main = window.main = new clazz(sender);
+    }
+  };
 })(this);
